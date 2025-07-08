639afb3c93a430f52cd78c5b495cb0cdbd988f7f
Aaron <115331438+Aaron2578@users.noreply.github.com>
2025-07-08 22:07:33 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import appRouter from "@/routers";
+import appRouter from "./routers"; // Adjust if using alias
 
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,7 +18,7 @@ if (!process.env.MONGODB_URI) throw new Error("Provide db url in env");
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
   console.log("Db connected");
-  app.listen(8, () => {
-    console.log("Server is listening in port 8");
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
   });
 });
